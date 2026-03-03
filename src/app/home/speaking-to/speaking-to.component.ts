a5abf360c6b2df4bf8f0923da1c7067233ddb4f6
NMN3151 <nipunmadushankaint@gmail.com>
2026-03-03 17:31:12 +0530

    added imagessss

--- src/app/home/speaking-to/speaking-to.component.ts
+++ src/app/home/speaking-to/speaking-to.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-speaking-to',
   templateUrl: './speaking-to.component.html',
   styleUrls: ['./speaking-to.component.css']
 })
-export class SpeakingToComponent {
+export class SpeakingToComponent implements OnInit {
+
+  ngOnInit(): void {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
 
 }
